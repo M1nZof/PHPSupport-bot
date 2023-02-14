086c8ab35ed87c5f802a0f736dac6c73c7b0d023
M1nZof <maxykl38@gmail.com>
2023-02-14 23:17:51 +0300

    Тестовая команда /start

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,8 @@
-# логика команды /start
+from telebot.types import Message
+
+from loader import bot
+
+
+@bot.message_handler(commands=['start'])
+def bot_start(message: Message):
+    bot.send_message(message.chat.id, "start")
