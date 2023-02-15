9d59452a136b8ae5979166818a0eb5969baf4f4a
Arkadiy <capark74@mail.ru>
2023-02-15 21:47:53 +0300

    изменения бота

---
+++ bot/contractor.py
@@ -0,0 +1,81 @@
+
+
+def fetch_completed_oreders():
+
+    completed_orders = [
+        {
+            'id': 151,
+            'id_customer': 25,
+            'id_contractor': 737812092,
+            'description': 'Добавить платёжную систему на сайт',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 155,
+            'id_customer': 25,
+            'id_contractor': 737812092,
+            'description': 'выгрузить товары с сайта в Excel-таблице',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 159,
+            'id_customer': 26,
+            'id_contractor': 737812092,
+            'description': 'загрузить 450 SKU на сайт из Excel таблицыт',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 161,
+            'id_customer': 22,
+            'id_contractor': 737812092,
+            'description': 'разместить баннер',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 171,
+            'id_customer': 29,
+            'id_contractor': 737812092,
+            'description': 'добавить платёжную систему на сайт',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 191,
+            'id_customer': 27,
+            'id_contractor': 737812092,
+            'description': 'добавить в интернет-магазин фильтр товаров по цвету',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+        {
+            'id': 195,
+            'id_customer': 29,
+            'id_contractor': 737812092,
+            'description': 'добавить в интернет-магазин фильтр товаров по цвету',
+            'completed': True,
+            'in_work': True,
+            'cost': 1500.00
+        },
+    ]
+    orders_to_message = list()
+    total_cost = 0;
+    for order in completed_orders:
+        orders_to_message.append(
+            f'описание: {order["description"]}, оплата: {order["cost"]} \n'            
+        )
+        total_cost += order['cost']
+    orders_to_message.append(
+        f'Общая сумма: {total_cost} \n'
+    )
+    return ''.join(orders_to_message)
+#print(fetch_completed_oreders())
--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,117 @@
+<<<<<<< Updated upstream
 from loader import updater
+=======
+# from loader import updater        # Будущий актуальный запуск
+#
+#
+# if __name__ == '__main__':
+#     updater.start_polling()
+from config import config
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+import contractor as ct
+
+ROLE, FREELANCE_START, CUSTOMER_START, CUSTOMER_SUBSCRIBE, LOCATION, BIO = range(6)
+
+
+def start(update, _):
+    reply_keyboard = [['Я разработчик', 'Я заказчик']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text(
+        'Я - бот по организации PHP фрилансеров. '
+        'Вы хотите быть фрилансером или заказчиком?\n\n'
+        'Команда /cancel, чтобы прекратить разговор',
+        reply_markup=markup_key
+    )
+    return ROLE
+
+
+def freelance_menu(update, _):
+    reply_keyboard = [['Помощь', 'Доступные заказы', 'Отчет']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text(
+        'Описание работы бота для фрилансера',
+        reply_markup=markup_key
+    )
+    return FREELANCE_START
+
+
+def freelance_get_orders(update, _):
+    update.message.reply_text(
+        'Вывод доступных заказов',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+
+
+def freelance_get_report(update, _):
+    update.message.reply_text(
+        'Отчет по выполненным работам \n'
+        f'{ct.fetch_completed_oreders()}',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+
+
+def customer_menu(update, _):
+    reply_keyboard = [['Оформить подписку', 'История заказов']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text(
+        'Здесь будет менюшка заказчика',
+        reply_markup=markup_key,
+    )
+    return CUSTOMER_START
+
+
+def customer_orders_history(update, _):
+    update.message.reply_text(
+        'Отчеты по выполненным и активным работам',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return ConversationHandler.END
+
+
+def subscribe(update, _):
+    reply_keyboard = [['Согласен', 'Не согласен']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text(
+        'Условия подписки',
+        reply_markup=markup_key,
+    )
+    return CUSTOMER_SUBSCRIBE
+
+
+def customer_place_order(update, _):
+    update.message.reply_text(
+        'Размещение заказа',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return ConversationHandler.END
+
+
+def customer_declined(update, _):
+    update.message.reply_text(
+        'Прощание с клиентом',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return ConversationHandler.END
+
+
+def cancel(update, _):
+    update.message.reply_text(
+        'Мое дело предложить - Ваше отказаться'
+        ' Будет скучно - пиши.',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
