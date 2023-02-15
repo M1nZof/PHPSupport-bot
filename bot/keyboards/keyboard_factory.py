2dbaa9ce1e9fe7f7e6f09b00d02e7fa6a561a19d
M1nZof <maxykl38@gmail.com>
2023-02-16 01:00:36 +0300

    Добавил Inline Keyboard фабрику

---
+++ bot/keyboards/keyboard_factory.py
@@ -0,0 +1,32 @@
+import math
+from abc import ABC
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+class ButtonFactory(ABC):
+    def get_inline_keyboard(self):
+        key_list = self.__dict__
+
+        inline_keyboard = []
+
+        for i_key in key_list.values():
+            for j_key, j_value in i_key.items():
+                inline_keyboard.append(
+                    InlineKeyboardButton(
+                        text=j_key, callback_data=j_value))
+
+        try:
+            if len(inline_keyboard) == any(inline_keyboard_len for inline_keyboard_len in range(1, 4)):
+                return inline_keyboard
+        finally:
+            if not any(isinstance(inline_keyboard, list) for _ in inline_keyboard):
+                return inline_keyboard
+
+        upd_inline_keyboard = []
+        index = 0
+        for _ in range(math.ceil(len(inline_keyboard) / 3)):
+            upd_inline_keyboard.append(inline_keyboard[index:index + 3])
+            index += 3
+        return upd_inline_keyboard
+
